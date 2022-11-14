a9858b2b5a25065b1670f342635cad63d5d7d70a
MinuMThomas <minumarytomas@gmail.com>
2022-11-14 21:54:30 +0000

    add just test

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,9 @@ describe ("Calculator", () => {
         test("should return 42 for 20 + 22",() => {
             expect(addition(20, 22)).toBe(42);
         })
+        test("should return 62 for 40 + 22",() => {
+            expect(addition(40, 22)).toBe(62);
+        })
     });
     describe("Substraction function", () => {
 
