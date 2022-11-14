fa2b40350bd2404008f9980f9394ea941556fdad
MinuMThomas <minumarytomas@gmail.com>
2022-11-14 20:25:20 +0000

    add test file

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,18 @@
+const addition = require("../calc");
+
+describe ("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22",() => {
+            except(addition(20, 22)).toBe(42);
+        })
+    });
+    describe("Substractio function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+})
