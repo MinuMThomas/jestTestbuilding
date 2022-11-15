d76b06129a7942fe8022d80a61bd81b9a34476d4
MinuMThomas <minumarytomas@gmail.com>
2022-11-15 21:55:16 +0000

    test showturns fn

--- scripts/game.js
+++ scripts/game.js
@@ -24,14 +24,38 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light")
+    }, 400);
+}
 
-module.exports = { game, newGame, showScore };
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns)
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns};
 /** why the curly braces? Well this is because we'll be exporting more than  
 one object and function from this file,  so we need to put them in curly braces. 
 We'll do exactly the same now as we  import this into our game.test.js file,  
